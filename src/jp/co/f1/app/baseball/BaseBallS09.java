cd15541cc65656054599e93e1d3eab32d003f651
kanda-it <kanda-it@192.168.1.161>
2023-07-28 10:19:00 +0900

    baseball step9

--- src/jp/co/f1/app/baseball/BaseBallS09.java
+++ src/jp/co/f1/app/baseball/BaseBallS09.java
@@ -37,30 +37,52 @@ public class BaseBallS09 {
 				int[] playerNum = new int[3];
 
 				while (strike != 3) {
-					System.out.println("");
-					System.out.print("3桁の数字を入力してください＞＞");
-					String num = sc.nextLine();
+					try {
+						System.out.println("");
+						System.out.print("3桁の数字を入力してください＞＞");
+						String num = sc.nextLine();
+						// 空文字の場合、以下のエラーメッセージを表示します
+						if (num.equals("")) {
+							System.out.println("何も入力されていません。ユニークな3桁の数字を入力してください。");
+							continue;
+						}
 
-					playerNum[0] = Integer.parseInt(num.substring(0, 1));
-					playerNum[1] = Integer.parseInt(num.substring(1, 2));
-					playerNum[2] = Integer.parseInt(num.substring(2));
+						// 3桁以外の桁数の数字が入力された場合、以下のエラーメッセージを表示します
+						if (num.length() != 3) {
+							System.out.println("3桁の数字ではありません。ユニークな3桁の数字を入力してください。");
+							continue;
+						}
 
-					if (checkDuplicate(playerNum)) {
-						count++;
-						strike = 0;
-						int ball = 0;
-						for (int i = 0; i < 3; i++) {
-							if (playerNum[i] == answer[i]) {
-								strike++;
-							}
-							for (int j = 0; j < 3; j++) {
-								if (playerNum[i] != answer[i] && playerNum[i] == answer[j]) {
-									ball++;
+						playerNum[0] = Integer.parseInt(num.substring(0, 1));
+						playerNum[1] = Integer.parseInt(num.substring(1, 2));
+						playerNum[2] = Integer.parseInt(num.substring(2));
+
+						if (checkDuplicate(playerNum)) {
+							count++;
+							strike = 0;
+							int ball = 0;
+							for (int i = 0; i < 3; i++) {
+								if (playerNum[i] == answer[i]) {
+									strike++;
+								}
+								for (int j = 0; j < 3; j++) {
+									if (playerNum[i] != answer[i] && playerNum[i] == answer[j]) {
+										ball++;
+									}
 								}
 							}
+							System.out.println("判定：" + strike + "ストライク、" + ball + "ボールです。");
 						}
-						System.out.println("判定：" + strike + "ストライク、" + ball + "ボールです。");
+						// 文字が含まれている場合、以下のエラーメッセージを表示します
+					} catch (NumberFormatException e) {
+						System.out.println("文字が入力されました。ユニークな3桁の数字を入力してください。");
+						continue;
+					} catch (ArrayIndexOutOfBoundsException e) {
+						System.out.println("配列のエラーが発生しました。");
+					} catch (Exception e) {
+						System.out.println(e + "という例外が発生しました。");
 					}
+
 				}
 				System.out.println(count + "回トライし、３桁数字を当てました。You Win!!");
 				break;
